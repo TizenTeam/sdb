c20bb912f4f715ecad4405cfdf4170984cf9c2ff
shingil.kang <shingil.kang@samsung.com>
2014-04-01 15:55:59 +0900

    SDB: Fixed a bug where completing remote path automatically is not worked.
    Change-Id: I35e4cf47bff26b009a630cf01295963dc1aeaefd
    Signed-off-by: shingil.kang <shingil.kang@samsung.com>

--- package/.sdb-completion.bash
+++ package/.sdb-completion.bash
@@ -1,9 +1,12 @@
 _sdb()
 {
-    #sdb path is defined in PATH environment variable
-    COMPREPLY=()
+    #check if the input is local or remote path.
+    INPUT_IS_PATH=0
 
+    #sdb path is defined in PATH environment variable
     SDB_PATH=$(eval eval echo \$\{COMP_WORDS\[0\]\})
+
+    COMPREPLY=()
 
     if [ ! -f ${SDB_PATH} ];
     then
@@ -14,6 +17,13 @@ _sdb()
     ARGS="autocomplete,${COMP_CWORD}"
 
     local IFS=$','
+
+    if [ -n ${COMP_WORDS[1]} ]; then
+        if [ "${COMP_WORDS[1]}" == "push" ] || [ "${COMP_WORDS[1]}" == "pull" ]; then
+	    INPUT_IS_PATH=1
+	fi
+    fi
+
     for ((i=1; i < $((${COMP_CWORD} + 1)) ; i++))
     do
         #processing for echo options
@@ -30,9 +40,12 @@ _sdb()
     next=($("${SDB_PATH}" ${ARGS}))
     local IFS=$'\n'
     COMPREPLY=(${next})
-#    COMPREPLY=($(compgen -W "${next}" -- ${cur}))
+
+    if [ $INPUT_IS_PATH == 0 ]; then 
+        COMPREPLY=( "${COMPREPLY[@]/%/ }" )   #add trailing space to each
+    fi
 
     return 0
 }
 
-complete -o filenames -F _sdb sdb
+complete -o nospace -F _sdb sdb
